d68b692e5af59964770efae905879d8cb2e1cf2d
agpiermarini <piermarini.andrew@gmail.com>
2018-07-02 15:30:08 -0600

    Update seed file

--- db/seeds/production/02_meals.js
+++ db/seeds/production/02_meals.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
-  .then(function () {
-      return Promise.all([
-        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [1, "Breakfast"]),
-        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [2, "Snack"]),
-        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [3, "Lunch"]),
-        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [4, "Dinner"])
-    ])
-  })
-}
+  return knex('meals').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('meals').insert([
+        {id: 1, colName: 'Breakfast'},
+        {id: 2, colName: 'Snack'},
+        {id: 3, colName: 'Lunch'}
+        {id: 4, colName: 'Dinner'}
+      ]);
+    });
+};
