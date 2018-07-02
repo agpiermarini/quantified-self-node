9a080c81659e80bc047c5a90b31c98b17f0b712b
agpiermarini <piermarini.andrew@gmail.com>
2018-07-02 13:26:22 -0600

    Remove comments from meal_foods migration

--- db/migrations/20180630150346_create-meal-foods.js
+++ db/migrations/20180630150346_create-meal-foods.js
@@ -11,19 +11,3 @@ exports.down = function(knex, Promise) {
   let dropTable = `DROP TABLE meal_foods`
   return knex.raw(dropTable);
 };
-
-
-// exports.up = function(knex, Promise) {
-//   let createTable = `CREATE TABLE meal_foods(
-//                       id SERIAL PRIMARY KEY NOT NULL,
-//                       meal_id INTEGER NOT NULL,
-//                       food_id INTEGER NOT NULL,
-//                       FOREIGN KEY (meal_id) REFERENCES meals(id),
-//                       FOREIGN KEY (food_id) REFERENCES foods(id))`
-//   return knex.raw(createTable);
-// };
-//
-// exports.down = function(knex, Promise) {
-//   let dropTable = `DROP TABLE meal_foods`
-//   return knex.raw(dropTable);
-// };
