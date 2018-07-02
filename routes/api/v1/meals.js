cfb49c2ae18aa8f9ee774affe85770c13915a8d9
agpiermarini <piermarini.andrew@gmail.com>
2018-07-01 20:40:28 -0600

    Add post route to meal endpoints

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -37,6 +37,28 @@ router.get('/:id/foods', function(req, res, next) {
         return res.status(200).json(foods.rows)
       }
     })
+});
+
+/* POST create joins record for existing food item and existing meal */
+router.post('/:meal_id/foods/:food_id', function(req, res, next) {
+  let meal_id = req.params.meal_id
+  let food_id = req.params.food_id
+  database.raw(`INSERT INTO meal_foods (meal_id, food_id) VALUES (?, ?)`, [meal_id, food_id])
+    .then(() => {
+      return database.raw(`SELECT m.name AS meal_name, f.name AS food_name
+                   FROM meals m
+                   INNER JOIN meal_foods mf ON m.id = mf.meal_id
+                   INNER JOIN foods f ON f.id = mf.food_id
+                   WHERE m.id=? AND f.id=?`, [meal_id, food_id])
+    })
+    .then((result) => {
+      if (!result.rows) {
+        return res.sendStatus(404);
+      } else {
+        let names = result.rows[0]
+        return res.status(200).json( { message: `Successfully added ${names.food_name} to ${names.meal_name}` } )
+      }
+    })
     .catch(err=>{
       return res.sendStatus(404);
     })
--- test/meals.test.js
+++ test/meals.test.js
@@ -78,152 +78,49 @@ describe('Meal endpoints', function() {
   });
 });
 
+describe('Meal endpoints', function() {
+  beforeEach(function(done) {
+    knex.seed.run()
+    .then(function() {
+      done();
+    });
+  });
 
-//   describe("GET /api/v1/foods", () => {
-//     it('returns all foods in the database', (done) => {
-//       chai.request(app)
-//       .get('/api/v1/foods')
-//       .end((err, res) => {
-//         expect(err).to.be.null;
-//         expect(res).to.have.status(200);
-//         expect(res.body.length).to.eql(3);
-//         expect(res.body[0].name).to.eq("Ramen");
-//         expect(res.body[0].calories).to.eq(650);
-//         expect(res.body[1].name).to.eq("Coffee");
-//         expect(res.body[1].calories).to.eq(50);
-//         expect(res.body[2].name).to.eq("Shumai");
-//         expect(res.body[2].calories).to.eq(400);
-//         done();
-//       })
-//     })
-//   })
-//
-//   describe("GET /api/v1/foods/:id", () => {
-//     it('returns food corresponding to :id', (done) => {
-//       chai.request(app)
-//       .get('/api/v1/foods/1')
-//       .end((err, res) => {
-//         expect(err).to.be.null;
-//         expect(res).to.have.status(200);
-//         expect(res.body.length).to.eql(1);
-//         expect(res.body[0].name).to.eq("Ramen");
-//         expect(res.body[0].calories).to.eq(650);
-//         done();
-//       })
-//     })
-//
-//     it('returns 404 for nonexisting record', (done) => {
-//       chai.request(app)
-//       .get('/api/v1/foods/99')
-//       .end((err, res) => {
-//         expect(res).to.have.status(404);
-//         done();
-//       })
-//     })
-//   })
-//
-//   describe("POST /api/v1/foods", () => {
-//     it('creates a new food object in the database', (done) => {
-//       chai.request(app)
-//       .post('/api/v1/foods')
-//       .send({ "food": { "name": "orange", "calories": 900} })
-//       .end((err, res) => {
-//         expect(err).to.be.null;
-//         expect(res).to.have.status(200);
-//         expect(res.body.name).to.eq("orange");
-//         expect(res.body.calories).to.eq(900);
-//         // expect(knex('foods').count('*')
-//         //     .then(foods => {
-//         //       console.log(foods[0]["count"]);
-//         //       return foods[0]["count"]
-//         //     })).to.eq('4');
-//         done();
-//       })
-//     })
-//
-//     it('fails to create record if name missing', (done) => {
-//       chai.request(app)
-//       .post('/api/v1/foods')
-//       .send({ "food": { "calories": 900} })
-//       .end((err, res) => {
-//         expect(res).to.have.status(404);
-//         done();
-//       })
-//     })
-//
-//     it('fails to create record if calories missing', (done) => {
-//       chai.request(app)
-//       .post('/api/v1/foods')
-//       .send({ "food": { "name": "orange"} })
-//       .end((err, res) => {
-//         expect(res).to.have.status(404);
-//         done();
-//       })
-//     })
-//   })
-//
-//   describe("PATCH /api/v1/foods/:id", () => {
-//     it('updates food object corresponding to :id', (done) => {
-//       chai.request(app)
-//       .patch('/api/v1/foods/1')
-//       .send({ "food": { "name": "orange", "calories": 900} })
-//       .end((err, res) => {
-//         expect(err).to.be.null;
-//         expect(res).to.have.status(200);
-//         expect(res.body.name).to.eq("orange");
-//         expect(res.body.calories).to.eq(900);
-//         done();
-//       })
-//     })
-//
-//     it('returns a 404 if name is missing', (done) => {
-//       chai.request(app)
-//       .patch('/api/v1/foods/1')
-//       .send({ "food": {  "calories": 900} })
-//       .end((err, res) => {
-//         expect(res).to.have.status(404);
-//         done();
-//       })
-//     })
-//
-//     it('returns a 404 if calories is missing', (done) => {
-//       chai.request(app)
-//       .patch('/api/v1/foods/1')
-//       .send({ "food": {  "name": "orange"} })
-//       .end((err, res) => {
-//         expect(res).to.have.status(404);
-//         done();
-//       })
-//     })
-//
-//     it('returns a 404 if record does not exist', (done) => {
-//       chai.request(app)
-//       .patch('/api/v1/foods/99')
-//       .send({ "food": {  "name": "orange", "calories": 100 } })
-//       .end((err, res) => {
-//         expect(res).to.have.status(404);
-//         done();
-//       })
-//     })
-//   })
-//
-//   describe("DELETE /api/v1/foods/:id", () => {
-//     it('deletes food object corresponding to :id', (done) => {
-//       chai.request(app)
-//       .delete('/api/v1/foods/1')
-//       .end((err, res) => {
-//         expect(res).to.have.status(204);
-//         done();
-//       })
-//     })
-//
-//     it('returns a 404 when trying to delete nonexisting record', (done) => {
-//       chai.request(app)
-//       .delete('/api/v1/foods/1')
-//       .end((err, res) => {
-//         expect(res).to.have.status(204);
-//         done();
-//       })
-//     })
-//   })
-// });
+  afterEach(function(done) {
+    knex.seed.run()
+    .then(function() {
+      done();
+    });
+  });
+
+  describe("POST /api/v1/meals/:meal_id/foods/:food_id", () => {
+    it("creates new record in joins table and returns a message", (done) => {
+      chai.request(app)
+      .post("/api/v1/meals/1/foods/3")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.eq("Successfully added Shumai to Breakfast");
+        done();
+      })
+    })
+
+    it("returns 404 for nonexistent meal", (done) => {
+      chai.request(app)
+      .get("/api/v1/meals/5/foods/3")
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      })
+    })
+
+    it("returns 404 for nonexistent food", (done) => {
+      chai.request(app)
+      .get("/api/v1/meals/1/foods/5")
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      })
+    })
+  });
+});
