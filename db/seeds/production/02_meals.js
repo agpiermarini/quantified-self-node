549d4f4d1e9b8fb1a9ab79ed783882bc976efd5c
agpiermarini <piermarini.andrew@gmail.com>
2018-07-02 15:17:13 -0600

    Revise seed

--- db/seeds/production/02_meals.js
+++ db/seeds/production/02_meals.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
   .then(function () {
       return Promise.all([
         knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [1, "Breakfast"]),
-        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [2, "Lunch"]),
-        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [3, "Snack"]),
+        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [2, "Snack"]),
+        knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [3, "Lunch"]),
         knex.raw('INSERT INTO meals (id, name) VALUES (?, ?)', [4, "Dinner"])
     ])
   })
