79a80201a55153a298b335dba59c26c57b02ec6a
agpiermarini <piermarini.andrew@gmail.com>
2018-07-16 16:26:51 -0600

    Move search url and parameter parsing to service

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -8,11 +8,7 @@ class recipesController {
 
 
   static async index (req, res, next) {
-    const searchUrl = "http://api.yummly.com/v1/api/recipes?_app_id=1cf459b5&_app_key=fcdc401b4e09697db2c9a0cf4297f53a"
-    let food = await Food.find(req)
-    let food_name = food.rows[0].name
-
-    let recipes = await Recipes.fetchRecipes(searchUrl, food_name)
+    let recipes = await Recipes.fetchRecipes(req)
 
     const formatRecipes = (recipes) => {
       let recipeList = recipes.map(recipe => {
--- services/recipes.js
+++ services/recipes.js
@@ -3,12 +3,16 @@ const fetch = require("node-fetch");
 
 
 class Recipes {
-  static fetchRecipes (searchUrl, food_name) {
-      return fetch(`${searchUrl}&q=${food_name}`)
-        .then(response => response.json())
-        .then(recipes => recipes.matches)
-        .catch(error => error)
-  }
+  static async fetchRecipes (req) {
+    const searchUrl = "http://api.yummly.com/v1/api/recipes?_app_id=1cf459b5&_app_key=fcdc401b4e09697db2c9a0cf4297f53a"
+    let food = await Food.find(req)
+    let food_name = food.rows[0].name
+
+    return fetch(`${searchUrl}&q=${food_name}`)
+      .then(response => response.json())
+      .then(recipes => recipes.matches)
+      .catch(error => error)
+    }
 }
 
 module.exports = Recipes
