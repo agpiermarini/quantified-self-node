b0b67fe88384768247d96d65467874d267c770c7
agpiermarini <piermarini.andrew@gmail.com>
2018-07-16 12:51:26 -0600

    Revise approach to attempt handrolling json

--- controllers/favoriteFoodsController.js
+++ controllers/favoriteFoodsController.js
@@ -8,29 +8,16 @@ class favoriteFoodsController {
 
   static async index (req, res, next) {
     const foodCounts = await MealFood.timesEaten()
-    console.log(foodCounts)
-    return foodCounts.rows ? res.status(200).json(foodCounts.rows) : res.sendStatus(404)
+    let favoriteFoods = foodCounts.rows.map(row => {
+      let favorite = {}
+      favorite["timesEaten"] = row.timeseaten
+      favorite["foods"] = [{}]
+      favorite["foods"][0]["name"] = row.name
+      favorite["foods"][0]["calories"] = row.calories
+      return favorite
+    }
+    return favoriteFoods ? res.status(200).json(favoriteFoods) : res.sendStatus(404)
   }
-
-  // static async show (req, res, next) {
-  //   const food = await Food.find (req)
-  //   return food.rows.length == 1 ? res.status(200).json(food.rows[0]) : res.sendStatus(404)
-  // }
-  //
-  // static async create (req, res, next) {
-  //   const food = await Food.create (req)
-  //   return food ? res.status(200).json(food.rows[0]) : res.sendStatus(404)
-  // }
-  //
-  // static async update (req, res, next) {
-  //   const food = await Food.update (req)
-  //   return food && food.rows.length == 1 ? res.status(200).json(food.rows[0]) : res.sendStatus(404)
-  // }
-  //
-  // static async delete  (req, res, next) {
-  //   const food = await Food.delete (req)
-  //   return food.rows ? res.sendStatus(204) : res.sendStatus(404)
-  // }
 }
 
 module.exports = favoriteFoodsController;
