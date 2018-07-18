59483f83c835e75809903dfa533b58d0e89618dc
agpiermarini <piermarini.andrew@gmail.com>
2018-07-18 12:04:51 -0600

    Add favoriteFoodsController. Add timesEaten query method to mealFoods model. Add timesEaten method to favoriteFoodsController to format json response

--- models/mealFood.js
+++ models/mealFood.js
@@ -38,7 +38,29 @@ class MealFood {
         .catch(err => null )
   }
 
+  static timesEaten () {
+    return database.raw(`SELECT count(foods.id) as timeseaten, foods.id,  foods.name, foods.calories, json_agg(json_build_object('name', meals.name)) as meal_names
+                         FROM meal_foods JOIN foods ON meal_foods.food_id = foods.id
+                         JOIN meals ON meal_foods.meal_id = meals.id
+                         GROUP BY meal_foods.food_id, foods.id, foods.name, foods.calories;`)
+    .then(foodCounts => foodCounts)
+  }
 }
 
 
 module.exports = MealFood
+
+
+
+// return database.raw(`SELECT count(meal_foods.food_id) as timesEaten,
+//                     to_json(array_agg(DISTINCT foods.*)) AS foods
+//                     FROM meal_foods LEFT OUTER JOIN foods ON meal_foods.food_id = foods.id
+//                     GROUP BY meal_foods.food_id, foods.name, foods.id;`)
+// return database.raw(`SELECT count(meal_foods.food_id) as timesEaten,
+//                     to_json(array_agg(DISTINCT (SELECT x FROM(SELECT foods.name, foods.calories) AS x))) AS foods
+//                     FROM meal_foods LEFT OUTER JOIN foods ON meal_foods.food_id = foods.id
+//                     GROUP BY meal_foods.food_id, foods.name, foods.id;`)
+// return database.raw(`SELECT count(meal_foods.food_id) as timesEaten,
+//                      COALESCE(json_agg((SELECT x FROM (SELECT foods.name, foods.calories) AS x)
+//                      ORDER BY foods.id GROUP BY foods.id) FILTER (WHERE foods.id IS NOT NULL), '[]') AS foods FROM meal_foods
+//                      JOIN foods ON meal_foods.food_id = foods.id GROUP BY meal_foods.food_id, foods.name, foods.id;`)
